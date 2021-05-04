ed9398b9b3518708011ceac56746aef6c1620ed1
chenc <chenchen3@fosun.com>
2021-05-05 00:54:14 +0800

    feat: init

--- rollup.config.js
+++ rollup.config.js
@@ -3,17 +3,28 @@ import typescript from '@rollup/plugin-typescript';
 import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
 import scss from 'rollup-plugin-scss'
-const fs=require('fs')
+
+const fs = require('fs')
 const env = process.env.NODE_ENV;
 const extensions = ['.js', '.jsx', '.ts', '.tsx'];
 
 export default {
     input: 'components/index.ts',
     output: [{
-        file: `lib/index${env === 'production' ? '.min' : ''}.js`,
-        format: 'umd',
+        file: `lib/index${env === 'production' ? '.min' : ''}.cjs.js`,
+        format: 'cjs',
         name: 'design',
-    }],
+    },
+        {
+            file: `lib/index${env === 'production' ? '.min' : ''}.esm.js`,
+            format: 'esm',
+            name: 'design',
+        },
+        {
+            file: `lib/index${env === 'production' ? '.min' : ''}.amd.js`,
+            format: 'amd',
+            name: 'design',
+        }],
     // 将模块视为外部模块，不会打包在库中
     external: ['react', 'react-dom'],
     // 插件
@@ -30,6 +41,7 @@ export default {
         commonjs(),
         typescript(),
         scss({
+            output: 'lib/bundle.css',
             prefix: `@import "../../styles/light-theme";`,
         })
     ],
