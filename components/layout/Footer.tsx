09752a5549c9dc75291d82216a3db772d2382fb8
chenc <chenchen3@fosun.com>
2021-05-18 18:19:15 +0800

    feat: 样式

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -3,8 +3,8 @@ import classNames from "classnames";
 
 const Footer: React.FC<classNameProps> = ({children, className}) => {
     return (
-        <footer className={classNames('zzf-layout-footer', className)}>
-            <section className={'zzf-box-responsive-main'}>
+        <footer className={'zzf-layout-footer'}>
+            <section className={classNames('zzf-box-responsive-main', className)}>
                 {children}
             </section>
         </footer>
