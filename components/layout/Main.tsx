568acb28133454ed8323a84a0fe9d3a625b5c29f
chenc <chenchen3@fosun.com>
2021-05-18 18:13:16 +0800

    feat: 样式

---
+++ components/layout/Main.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Main: React.FC = ({children}) => {
+    return (
+        <main className={'zzf-box-responsive-main'}>{children}</main>
+    );
+}
+
+export default Main;
