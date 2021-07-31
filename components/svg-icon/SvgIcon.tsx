0e39eb0eefd62d848d475f0779310b81a7a0c733
chenc <git@zzfzzf.com>
2021-07-31 16:46:11 +0800

    feat: 添加Icon组件

---
+++ components/svg-icon/SvgIcon.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import classNames from "classnames";
+
+interface SvgIconProps {
+    name: string;
+    onClick?: () => void;
+    color?: string;
+    className?: string;
+    size?: number;
+}
+
+const SvgIcon: React.FC<SvgIconProps> = ({name, color, size, onClick, className}) => {
+    return (
+        <svg
+            onClick={onClick}
+            className={classNames('zzf-icon', className)}
+            style={{color, fontSize: size && `${size}px`}}
+        >
+            <use xlinkHref={`#icon-${name}`}/>
+        </svg>
+    );
+}
+
+export default SvgIcon;
