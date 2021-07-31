978cfdd94e717f55528c1b03cfae0dc9fffe2e6c
chenc <chenchen3@fosun.com>
2021-07-31 23:38:47 +0800

    feat: 修改card组件

--- components/card/card.tsx
+++ components/card/card.tsx
@@ -1,18 +1,19 @@
 import React, {ReactNode} from 'react';
 import classNames from "classnames";
+import {SvgIcon} from "../svg-icon";
 
-interface cl {
-    className?: string
-}
-
-interface CardProps extends cl {
+interface CardProps extends classNameProps {
     title?: ReactNode
+    icon: string
 }
 
-const Card: React.FC<CardProps> = ({children, title, className}) => {
+const Card: React.FC<CardProps> = ({children,icon, title, className}) => {
     return (
         <section className={classNames('zzf-card', className)}>
-            <header>{title}</header>
+            <header>
+                <SvgIcon size={20} name={icon}/>
+                <span className="ml-3">{title}</span>
+            </header>
             {children}
         </section>
     );
