11c3e6ffed60f6cb8697c700452073ac29c00646
cc <cc@zzfzzf.com>
2021-10-25 21:24:02 +0800

    feat: scss未编译

--- rollup.config.js
+++ rollup.config.js
@@ -38,7 +38,7 @@ export default {
         }),
         copy({
             targets: [
-                {src: ['components/styles/_variable_dark.scss','components/styles/_variable_light.scss','components/styles/_theme.scss'], dest: 'dist'},
+                {src: ['components/styles/variable_dark.scss','components/styles/variable_light.scss','components/styles/theme.scss'], dest: 'dist'},
             ]
         })
     ],
