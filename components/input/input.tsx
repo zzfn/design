3280a67e28967026f6fc81cd602750c6caba1d72
cc <cc@zzfzzf.com>
2021-11-06 13:49:15 +0800

    fix: card button input UI

--- components/input/input.tsx
+++ components/input/input.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import classNames from "classnames";
 
-const Input: React.FC<classNameProps> = ({children, className}) => {
+interface InputType extends classNameProps {
+    type?: string;
+    autoFocus?: boolean;
+    placeholder?: string;
+    value?: string;
+    onChange?: (value: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+const Input: React.FC<InputType> = (props) => {
+    const {type = 'text', className, autoFocus = false, placeholder, value, onChange} = props;
+
     return (
-        <input className={classNames('zzf-ipt', className)}/>
+        <input value={value} onChange={onChange} placeholder={placeholder} autoFocus={autoFocus} type={type}
+               className={classNames('zzf-ipt', className)}/>
     );
 }
 
