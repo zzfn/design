eb5108b9353acac9f64bbb3d788f3c54ada86a14
cc <44751363+zzfn@users.noreply.github.com>
2022-07-09 19:18:34 +0800

    chore: style

---
+++ components/alert/alert.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import classNames from 'classnames'
+
+interface ButtonProps {
+  children: React.ReactNode;
+  className?: string;
+  type?: string;
+}
+
+const Alert: React.FC<ButtonProps> = ({ children, className, type }) => {
+  return (
+    <div className={classNames('zzf-alert', type && `zzf-alert-${type}`, className)}>
+      {children}
+    </div>
+  )
+}
+
+export default Alert
