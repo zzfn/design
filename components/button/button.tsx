326219c9165b8402b6b00df2fa462c169980b7e0
zzfn <44751363+zzfn@users.noreply.github.com>
2022-07-14 22:52:51 +0800

    fix: performance

--- components/button/button.tsx
+++ components/button/button.tsx
@@ -4,12 +4,13 @@ import classNames from 'classnames';
 interface ButtonProps {
   children: React.ReactNode;
   className?: string;
+  theme?: 'light' | 'solid';
   onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void;
 }
 
-const Button: React.FC<ButtonProps> = ({ children, className, onClick }) => {
+const Button: React.FC<ButtonProps> = ({ children, theme = 'light', className, onClick }) => {
   return (
-    <button onClick={onClick} className={classNames('zzf-btn', className)}>
+    <button onClick={onClick} className={classNames('zzf-btn',`zzf-btn-${theme}`, className)}>
       {children}
     </button>
   );
--- components/modal/content.tsx
+++ components/modal/content.tsx
@@ -1,52 +1,57 @@
 import React from 'react';
+import { Button } from '../button';
+import { Space } from '../space';
 
 export default function Content(props: any) {
   return (
     props.visible && (
-      <div className={'zzf-modal-container'}>
-        <header className={'zzf-modal-header'}>
-          <div>{props.title}</div>
-          <svg
-            onClick={() => props.close()}
-            className={'zzf-model-close'}
-            width='1em'
-            height='1em'
-            viewBox='0 0 48 48'
-            fill='currentColor'
-            xmlns='http://www.w3.org/2000/svg'
-          >
-            <path
-              d='M8 8L40 40'
-              stroke='currentColor'
-              strokeWidth='4'
-              strokeLinecap='round'
-              strokeLinejoin='round'
-            />
-            <path
-              d='M8 40L40 8'
-              stroke='currentColor'
-              strokeWidth='4'
-              strokeLinecap='round'
-              strokeLinejoin='round'
-            />
-          </svg>
-        </header>
-        <div>{props.children}</div>
-        <div>
-          <button
-            onClick={() => {
-              props.close();
-            }}
-          >
-            取消
-          </button>
-          <button
-            onClick={() => {
-              props.close();
-            }}
-          >
-            确定
-          </button>
+      <div className={'zzf-modal-mask'}>
+        <div className={'zzf-modal-container'}>
+          <header className={'zzf-modal-header'}>
+            <div>{props.title}</div>
+            <svg
+              onClick={() => props.close()}
+              className={'zzf-model-close'}
+              width='1em'
+              height='1em'
+              viewBox='0 0 48 48'
+              fill='currentColor'
+              xmlns='http://www.w3.org/2000/svg'
+            >
+              <path
+                d='M8 8L40 40'
+                stroke='currentColor'
+                strokeWidth='4'
+                strokeLinecap='round'
+                strokeLinejoin='round'
+              />
+              <path
+                d='M8 40L40 8'
+                stroke='currentColor'
+                strokeWidth='4'
+                strokeLinecap='round'
+                strokeLinejoin='round'
+              />
+            </svg>
+          </header>
+          <div>{props.children}</div>
+          <footer className={'zzf-modal-footer'}>
+            <Button
+              onClick={() => {
+                props.close();
+              }}
+            >
+              取消
+            </Button>
+            <Button
+                theme={'solid'}
+              onClick={() => {
+                props.close();
+              }}
+            >
+              确定
+            </Button>
+          </footer>
         </div>
       </div>
     )
