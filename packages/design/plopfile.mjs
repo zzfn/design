1209eadfe6b57679b41e6217e2ea7646754d8a66
cc <44751363+zzfn@users.noreply.github.com>
2022-07-22 00:21:36 +0800

    chore: plob

---
+++ packages/design/plopfile.mjs
@@ -0,0 +1,35 @@
+export default function (plop) {
+  plop.setHelper('upperCase', (txt) => txt.toUpperCase());
+  plop.setGenerator('controller', {
+    description: 'application controller logic',
+    prompts: [
+      {
+        type: 'input',
+        name: 'name',
+        message: 'controller name please',
+      },
+    ],
+    actions: [
+      {
+        type: 'add',
+        path: 'components/{{name}}/index.tsx',
+        templateFile: 'templates/index.ts.hbs',
+      },
+      {
+        type: 'add',
+        path: 'components/{{name}}/{{properCase name}}.tsx',
+        templateFile: 'templates/index.tsx.hbs',
+      },
+      {
+        type: 'add',
+        path: 'components/{{name}}/style/index.ts',
+        templateFile: 'templates/style.index.ts.hbs',
+      },
+      {
+        type: 'add',
+        path: 'components/{{name}}/style/{{name}}.scss',
+        templateFile: 'templates/style.index.scss.hbs',
+      },
+    ],
+  });
+}
